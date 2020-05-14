c72ed57eddccca0d156c6b5b74ab537ffcc639a2
sousnein <56271425+sousnein@users.noreply.github.com>
2020-05-14 12:04:10 +0300

    Basics

---
+++ KotlinLearn/src/Hello.kt
@@ -0,0 +1,9 @@
+fun main(args:Array<String> ) {
+    println("Hello")
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+  print( sum(5,5))
+
+}
---
+++ KotlinLearn/src/Types.kt
@@ -0,0 +1,10 @@
+fun main(args:Array<String>){
+var experiencePoints:Int = 50;
+    print(experiencePoints)
+    var lvlup:Int=1;
+    if (experiencePoints>20)
+        lvlup+=1;
+    println()
+    print(lvlup)
+    var be:Boolean = true
+}
